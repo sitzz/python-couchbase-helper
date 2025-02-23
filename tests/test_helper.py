9ff6fdf3fd6028e7a745691b6448f4655a87911c
Thomas 'sitzz' Vang <8313369+sitzz@users.noreply.github.com>
2025-02-23 12:16:40 +0100

    chore:added tests for N1ql class

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,7 +6,7 @@ import pytest
 from couchbase_helper.protocols import SessionProt
 from couchbase_helper import Timeout
 
-from .couchbase_session import instance, session
+from .couchbase_session import cb_instance, session
 from .helpers import get_item, get_items
 
 
@@ -49,41 +49,41 @@ def test_connect():
 ##################################################
 def test_insert():
     key, document = get_item()
-    assert instance().insert(key, document)
+    assert cb_instance().insert(key, document)
 
 
 def test_insert_duplicate():
     key, document = get_item()
-    assert not instance().insert(key, document)
+    assert not cb_instance().insert(key, document)
 
 
 def test_insert_multi():
     documents_raw = get_items(100)
     documents = {key: document for key, document in documents_raw}
-    assert instance().insert_multi(documents)
+    assert cb_instance().insert_multi(documents)
 
 
 def test_upsert_existing():
     key, document = get_item()
-    assert instance().upsert(key, document)
+    assert cb_instance().upsert(key, document)
 
 
 def test_upsert():
     key, document = get_item("second")
-    assert instance().upsert(key, document)
+    assert cb_instance().upsert(key, document)
 
 
 def test_upsert_multi():
     documents_raw = get_items(100)
     documents = {key: document for key, document in documents_raw}
-    assert instance().upsert_multi(documents)
+    assert cb_instance().upsert_multi(documents)
 
 
 @pytest.mark.order(after="test_insert")
 def test_update():
     key, document = get_item()
     document["update"] = True
-    assert instance().upsert(key, document)
+    assert cb_instance().upsert(key, document)
 
 
 ##################################################
@@ -91,28 +91,28 @@ def test_update():
 ##################################################
 def test_get():
     key, document = get_item("get_test")
-    instance().insert(key, document)
-    document_fetched = instance().get(key)
+    cb_instance().insert(key, document)
+    document_fetched = cb_instance().get(key)
 
     assert json.dumps(document) == json.dumps(document_fetched)
-    assert instance().remove(key)
+    assert cb_instance().remove(key)
 
 
 def test_get_not_found():
     key, _ = get_item("get_not_found_test")
-    assert not instance().get(key)
+    assert not cb_instance().get(key)
 
 
 def test_get_multi():
     documents_raw = get_items(100, "get_multi_test")
     documents = {key: document for key, document in documents_raw}
-    instance().insert_multi(documents)
+    cb_instance().insert_multi(documents)
 
     keys = [key for key, _ in documents_raw]
-    documents_fetched = instance().get_multi(keys, raw=True)
+    documents_fetched = cb_instance().get_multi(keys, raw=True)
     assert len(documents) == len(documents_fetched)
     assert all(doc.key in keys for doc in documents_fetched)
-    assert instance().remove_multi(keys)
+    assert cb_instance().remove_multi(keys)
 
 
 ##################################################
@@ -121,28 +121,28 @@ def test_get_multi():
 @pytest.mark.order(after="test_insert")
 def test_remove():
     key, document = get_item()
-    assert instance().remove(key, document)
+    assert cb_instance().remove(key, document)
 
 
 @pytest.mark.order(after="test_upsert")
 def test_remove_second():
     key, document = get_item("second")
-    assert instance().remove(key, document)
+    assert cb_instance().remove(key, document)
 
 
 @pytest.mark.order(after="test_remove")
 def test_remove_not_found():
     key, document = get_item()
-    assert not instance().remove(key, document)
+    assert not cb_instance().remove(key, document)
 
 
 @pytest.mark.order(after="test_insert_multi")
 def test_remove_multi():
     keys = [key for key, _ in get_items(100)]
-    assert instance().remove_multi(keys)
+    assert cb_instance().remove_multi(keys)
 
 
 @pytest.mark.order(after="test_remove_multi")
 def test_remove_multi_not_found():
     keys = [key for key, _ in get_items(100)]
-    assert not instance().remove_multi(keys)
+    assert not cb_instance().remove_multi(keys)
