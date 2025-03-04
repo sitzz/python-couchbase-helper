595fbd54342af80c3a1020d5b7d29896ad6ae5ba
Thomas Vang Johansen <8313369+sitzz@users.noreply.github.com>
2025-03-04 09:05:39 +0100

    chore:bumped version

--- couchbase_helper/__init__.py
+++ couchbase_helper/__init__.py
@@ -4,4 +4,4 @@ from .timeout import Timeout
 
 __all__ = ["CouchbaseHelper", "Session", "Timeout"]
 __author__ = "Thomas 'sitzz' Vang <sitzzdk@gmail.com>"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
